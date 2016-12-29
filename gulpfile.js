7c0de267e7a914c14c314f96708b45021046b1cf
caixiaowei08 <caixiaowei08@163.com>
2016-12-30 00:36:14 +0800

    add base function

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/12/30.
+ */
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('default', function() {
+    // 将你的默认的任务代码放在这
+
+
+});
+
+gulp.task('default',['','']);
